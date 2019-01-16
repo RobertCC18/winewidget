0bd907d8b5653a82289263fa0039f6cc522fed2e
Robert Crawford <robert@robsclan.com>
2019-01-16 15:13:18 -0500

    Merge JS files
    merger loader and core JS files

--- widget-loader.js
+++ widget-loader.js
@@ -1,6 +1,57 @@
 jQuery(document).ready(function($) {
     $.get( "https://winewidget.herokuapp.com/suggest?url=" + window.location.href, function( data ) {
         $( ".winewidget-cont" ).html( data );
-  console.log( "Load was performed." );
+        console.log( "Load was performed." );
+         $("#wineSlider").lightSlider({
+        item: 1,
+        autoWidth: false,
+        slideMove: 1, // slidemove will be 1 if loop is true
+        slideMargin: 10,
+ 
+        addClass: '',
+        mode: "slide",
+        useCSS: true,
+        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+        easing: 'linear', //'for jquery animation',////
+ 
+        speed: 400, //ms'
+        auto: false,
+        loop: false,
+        slideEndAnimation: true,
+        pause: 2000,
+ 
+        keyPress: false,
+        controls: true,
+        prevHtml: '<span class="arrow"><</span>',
+        nextHtml: '<span class="arrow">></span>',
+ 
+        rtl:false,
+        adaptiveHeight:false,
+ 
+        vertical:false,
+        verticalHeight:500,
+        vThumbWidth:100,
+ 
+        thumbItem:10,
+        pager: true,
+        gallery: false,
+        galleryMargin: 5,
+        thumbMargin: 5,
+        currentPagerPosition: 'middle',
+ 
+        enableTouch:true,
+        enableDrag:true,
+        freeMove:true,
+        swipeThreshold: 40,
+ 
+        responsive : [],
+ 
+        onBeforeStart: function (el) {},
+        onSliderLoad: function (el) {},
+        onBeforeSlide: function (el) {},
+        onAfterSlide: function (el) {},
+        onBeforeNextSlide: function (el) {},
+        onBeforePrevSlide: function (el) {}
+    });
 });
 });
