bb570753be34cc10c5fbeb77da65fc8ab9c38cbe
Robert Crawford <robert@robsclan.com>
2019-04-04 09:27:28 -0400

    Updates

--- widget-loader.js
+++ widget-loader.js
@@ -2,7 +2,8 @@ jQuery(document).ready(function($) {
     $.get( "https://winewidget.herokuapp.com/suggest?url=https://therecipecritic.com/skillet-garlic-butter-herb-steak-and-potatoes/", function( data ) {
         $( ".winewidget-cont" ).html( data );
         console.log( "Load was performed." );
-         $("#wineSlider").lightSlider({
+         $(document).ready(function() {
+    $("#wineSlider").lightSlider({
         item: 1,
         autoWidth: false,
         slideMove: 1, // slidemove will be 1 if loop is true
@@ -22,14 +23,14 @@ jQuery(document).ready(function($) {
  
         keyPress: false,
         controls: true,
-        prevHtml: '<span class="arrow"><</span>',
-        nextHtml: '<span class="arrow">></span>',
- 
+        prevHtml: '<span class="_control"><</span>',
+        nextHtml: '<span class="_control">></span>',
+        
         rtl:false,
         adaptiveHeight:false,
  
         vertical:false,
-        verticalHeight:500,
+        verticalHeight:800,
         vThumbWidth:100,
  
         thumbItem:10,
@@ -54,4 +55,96 @@ jQuery(document).ready(function($) {
         onBeforePrevSlide: function (el) {}
     });
 });
+
+$("._popup_bottom_slider").lightSlider({
+        item: 3,
+        autoWidth: false,
+        slideMove: 1, // slidemove will be 1 if loop is true
+        slideMargin: 1,
+ 
+        addClass: '',
+        mode: "slide",
+        useCSS: true,
+        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+        easing: 'linear', //'for jquery animation',////
+ 
+        speed: 400, //ms'
+        auto: false,
+        loop: false,
+        slideEndAnimation: true,
+        pause: 2000,
+ 
+        keyPress: false,
+        controls: true,
+        prevHtml: '<span class="_control"><</span>',
+        nextHtml: '<span class="_control">></span>',
+        
+        rtl:false,
+        adaptiveHeight:false,
+ 
+        vertical:false,
+        verticalHeight:800,
+        vThumbWidth:100,
+ 
+        thumbItem:10,
+        pager: true,
+        gallery: false,
+        galleryMargin: 5,
+        thumbMargin: 5,
+        currentPagerPosition: 'middle',
+ 
+        enableTouch:true,
+        enableDrag:true,
+        freeMove:true,
+        swipeThreshold: 40,
+ 
+        responsive : [],
+ 
+        onBeforeStart: function (el) {},
+        onSliderLoad: function (el) {
+
+        },
+        onBeforeSlide: function (el) {},
+        onAfterSlide: function (el) {},
+        onBeforeNextSlide: function (el) {},
+        onBeforePrevSlide: function (el) {}
+    });
+
+
+
+
+/*$('._popup_bottom_slider').slick({
+  dots: true,
+  arrows: true,
+   centerMode: false,
+  centerPadding: '10px',
+   slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+}); */
+
+
+});
+});
+$('._popup_cont').on('show.bs.modal', function () {
+  $('._popup_bottom_slider').resize();
+
 });
